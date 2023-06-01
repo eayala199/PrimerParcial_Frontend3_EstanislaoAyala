ac8b577d759a899f23671d1c43d1400d68be4617
eayala199 <estanilaoa@gmail.com>
2023-05-31 22:28:24 -0300

    Examen terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,60 @@
-
+import { useState } from 'react'
 import './App.css'
 import Card from './components/Card'
 
 function App() {
 
+  const [nombre,setNombre] = useState("");
+  const [animal,setAnimal] = useState("");
+  const [show, setShow] = useState(false);
 
+  const handlerEnviar=(event)=>{
+    event.preventDefault();
+    if(nombre.length <3 || nombre.length === " " || animal.length <6) {
+        alert("Por favor chequea que la informacion sea correcta")
+    } else{
+      setShow(true);
+    }
+}
+
+  
   return (
     <>
       <div>
-        <h1>Carga de Estudiantes</h1>
-        <form></form>
-        <Card/>
+        <h1>Elige tu animal favorito:</h1>
+        <form className='form1' onSubmit={handlerEnviar}>
+        <div>
+        <label htmlFor= 'name'>
+            Nombre: {" "}
+        </label>
+        <input id='name' 
+        type= 'text'
+        placeholder='Escriba su nombre'
+        value={nombre}
+        onChange={e => setNombre(e.target.value)}
+        />
+        </div>
+        <br/>
+        <div>
+        <label htmlFor= 'animal'>
+            Animal: {" "}
+        </label>
+        <input id='animal' 
+        type= 'text'
+        placeholder='Escriba su animal favorito'
+        value={animal}
+        onChange={e => setAnimal(e.target.value)}
+        />
+        </div>
+        <br/>
+        <button className='boton' type='submit' onClick={() => {
+          setShow(show);
+        }}>Enviar</button>
+        </form>
+        <br/>
+        {show && (
+          <Card nombre={nombre} animal={animal} />
+        )}
       </div>
     </>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({nombre, animal}) => {
   return (
-    <div>Hola , soy una tarjeta</div>
+    <div className='card'>
+      <h2>Completaste los datos correctamente!!!</h2>
+      <h3>Nombre: {nombre}</h3>
+      <h3>Animal: {animal}</h3>
+    </div>
   )
 }
 
